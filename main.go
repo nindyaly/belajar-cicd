df810ff1be84b7a717e3a471288d3e74467e99e4
lintangp <44935511+lintangp@users.noreply.github.com>
2022-06-15 20:35:52 +0700

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,4 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("listening on port 3000...")
 	http.ListenAndServe(":3000", nil)
-	fmt.Println("Hello World!")
-	fmt.Println("Hello semuaa!")
 }
